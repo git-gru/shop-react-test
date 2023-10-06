b35ce71cf137c37945fdfb7b647ac16bfca5430d
GRU <new-comet-dev@outlook.com>
2023-10-05 19:10:56 -0700

    add mockdata for collections

---
+++ src/data/mockdata.js
@@ -0,0 +1,59 @@
+import CollectionImage1 from "../assets/images/collection1.png";
+import CollectionImage2 from "../assets/images/collection2.png";
+import CollectionImage3 from "../assets/images/collection3.png";
+import CollectionImage4 from "../assets/images/collection4.png";
+import CollectionImage5 from "../assets/images/collection5.png";
+import CollectionImage6 from "../assets/images/collection6.png";
+import CollectionImage7 from "../assets/images/collection7.png";
+import CollectionImage8 from "../assets/images/collection8.png";
+
+export const shopFeaturedCollections = [
+  {
+    title: "The Great Outdoors",
+    description:
+      "Whether hiking, fishing, or relaxing around the fire, be prepared for your next adventure.",
+    image: CollectionImage1,
+  },
+  {
+    title: "The Taste of Fall",
+    description:
+      "Shop cooking and baking essentials for whisking up your favorite fall recipes.",
+    image: CollectionImage2,
+  },
+  {
+    title: "Bold Brews and Blends",
+    description:
+      "Put purpose behind your purchase with tea and coffee brands that celebrate life and freedom.",
+    image: CollectionImage3,
+  },
+  {
+    title: "Tailgating Edition",
+    description:
+      "Gear up with all essentials you need to take the Hass out of game day prep.",
+    image: CollectionImage4,
+  },
+  {
+    title: "Health & Wellness",
+    description:
+      "Empower your health products and essentials designed to nourish and energize.",
+    image: CollectionImage5,
+  },
+  {
+    title: "Kitchen Staples",
+    description:
+      "Browse meats, sauces, snacks and more from businesses that share your values.",
+    image: CollectionImage6,
+  },
+  {
+    title: "Fall’s Must-Have Accessories",
+    description:
+      "Make a statement this fall with unique pieces and accessories from quality American brands.",
+    image: CollectionImage7,
+  },
+  {
+    title: "Men's Fall Clothing",
+    description:
+      "Shop patriot-loving businesses for high-quality men’s clothing and apparel.",
+    image: CollectionImage8,
+  },
+];
